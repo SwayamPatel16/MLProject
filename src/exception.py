05938f6c8b3ec238001638f94964a23c148e63f9
SwayamPatel16 <swayampatel741@gmail.com>
2025-10-25 22:00:24 +0530

    EDA and Problem Statement

--- src/exception.py
+++ src/exception.py
@@ -1,20 +1,23 @@
 import sys
-import logging
+from src.logger import logging
 
-def error_message_detail(error, error_detail:sys):
-    _,_,exc_tb = error_detail.exc_info()
+# tells what happened and where
 
-    file_name = exc_tb.tb_frame.f_code.co_filename
+#accident investigator
+def error_message_detail(error, error_detail:sys): #sys->imports investigators toolbox
+    _,_,exc_tb = error_detail.exc_info() #it uses sys tool to get the last exceptions info. we just care bout traceback
+
+    file_name = exc_tb.tb_frame.f_code.co_filename #finding the exact file name of the accident
     error_message = "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
         file_name, exc_tb.tb_lineno,str(error))
-
+   #it takes file name the exact line number and the original msg error
     return error_message
     
 
 class CustomException(Exception):
-    def __init__(self,error_message,error_detail:sys):
-        super().__init__(error_message)
-        self.error_message=error_message_detail(error_message,error_detail=error_detail)
+    def __init__(self,error_message,error_detail:sys): #instruction to filling out the form.  it reqs the og error msg and investigators toolbox(error detail)
+        super().__init__(error_message) #its like writing the og ouch msg at thr top of the form just for reference
+        self.error_message=error_message_detail(error_message,error_detail=error_detail) #calls the main func
 
     def __str__(self):
         return self.error_message
@@ -26,4 +29,6 @@ if __name__ == "__main__":
         a = 1/0
     except Exception as e:
         logging.info("Divide by zero")
-        raise CustomException(e,sys)
+        raise CustomException(e,sys) # 1. dont ignore this simple ouch error 2. i want to raise a new better error 3. creating a new customexp giving it the og ouch and the investigators toolbox.
+    #This triggers the def __init__() which calls the investigator func who builds detialed report. the raise command then stops the program and shows that new detailed report to the user
+    
